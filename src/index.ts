93a4048e0b3977210d577294297ef1486c810d04
beary <bearyme@outlook.com>
2019-01-31 20:04:12 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { APIGatewayEvent, Context, APIGatewayProxyResult } from 'aws-lambda'
+
+type AsyncHandler = (event: APIGatewayEvent, context: Context) => APIGatewayProxyResult
+
+export class BaseError extends Error {
+  public statusCode: number
+  constructor(message?: string) {
+    super(message)
+    this.statusCode = 400 // default statusCode
+  }
+}
+
+export const withCatch = (fn: AsyncHandler) =>
+  async (event: APIGatewayEvent, context: Context) => {
+    let res: APIGatewayProxyResult
+    try {
+      res = await fn(event, context)
+    } catch (error) {
+      res = {
+        body: `{"message":"${error.message}"}`,
+        statusCode: error instanceof BaseError ? error.statusCode : 500
+      }
+    }
+    return res
+  }
